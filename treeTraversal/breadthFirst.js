9a19d963be8dd7f1012463ff30f6c85aadfc8e78
SoniaDumitru <soni.dumitru@gmail.com>
2020-02-08 16:39:24 -0600

    Update breadthFirst.js

--- treeTraversal/breadthFirst.js
+++ treeTraversal/breadthFirst.js
@@ -12,7 +12,7 @@ a,b,c,d,e,f,g,h,i,j,k,l,m
       (k)(l)         (m)
 */
 
-// start the queue with a node execute the callback function on that and push all the children of that node in a queue
+// start the queue with a node, execute the callback function on that node and push all the children of that node in a queue
 const beadthFist = (node, callback) => {
 
   const queue = [node]
